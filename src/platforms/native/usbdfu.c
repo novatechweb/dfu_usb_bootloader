9d5f7b156b00e006133c0f34501567a584bf0b62
Joseph A. Lutz <joseph.lutz@novatechweb.com>
2015-02-26 09:52:58 -0600

    Add board support for 129 and 134 boards
    Rearanged some of the code and made the port pins more straight forward
    
    Signed-off-by: Joseph A. Lutz <joseph.lutz@novatechweb.com>

--- src/platforms/native/usbdfu.c
+++ src/platforms/native/usbdfu.c
@@ -29,127 +29,89 @@
 const volatile char __attribute__((__section__(".serialno"))) board_serial_no[16] = "SERIAL NUM HERE";
 uint32_t app_address = 0x08002000;
 
+#define SERIAL_NUM_NUM_CHARS 3
+const char serial_board_serial_numbers[][SERIAL_NUM_NUM_CHARS] = {
+	"124",
+	"129",
+	"134",
+};
+
 struct platform_output_pins_t {
 	const uint32_t port;
 	const uint16_t pin;
-	const uint8_t cnf;
 };
 
 struct platform_uart_t {
-	uint32_t usart;
 	struct platform_output_pins_t tx;
 	struct platform_output_pins_t rts;
 	struct platform_output_pins_t dtr;
 };
 
 const struct platform_uart_t uarts[] = {{
-	.usart = USART1,
+	// USART1,
 	.tx =  {	// TX
 		.port = GPIO_BANK_USART1_TX,
 		.pin = GPIO_USART1_TX,
-		.cnf = GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
 	},
 	.rts = {	// RTS
 		.port = GPIOC,
 		.pin = GPIO9,
-		.cnf = GPIO_CNF_OUTPUT_PUSHPULL,
 	},
 	.dtr = {	// DTR
 		.port = GPIOC,
 		.pin = GPIO12,
-		.cnf = GPIO_CNF_OUTPUT_PUSHPULL,
 	},
 },{
-	.usart = USART2,
+	// USART2,
 	.tx =  {	// TX
 		.port = GPIO_BANK_USART2_TX,
 		.pin = GPIO_USART2_TX,
-		.cnf = GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
 	},
 	.rts = {	// RTS
 		.port = GPIO_BANK_USART2_RTS,
 		.pin = GPIO_USART2_RTS,
-		.cnf = GPIO_CNF_OUTPUT_PUSHPULL,
 	},
 	.dtr = {	// DTR
 		.port = GPIOC,
 		.pin = GPIO2,
-		.cnf = GPIO_CNF_OUTPUT_PUSHPULL,
 	},
 },{
-	.usart = USART3,
+	// USART3,
 	.tx =  {	// TX
 		.port = GPIO_BANK_USART3_TX,
 		.pin = GPIO_USART3_TX,
-		.cnf = GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
 	},
 	.rts = {	// RTS
 		.port = GPIO_BANK_USART3_RTS,
 		.pin = GPIO_USART3_RTS,
-		.cnf = GPIO_CNF_OUTPUT_PUSHPULL,
 	},
 	.dtr = {	// DTR
 		.port = GPIOC,
 		.pin = GPIO5,
-		.cnf = GPIO_CNF_OUTPUT_PUSHPULL,
 	},
 },{
-	.usart = UART4,
+	// UART4,
 	.tx =  {	// TX
 		.port = GPIO_BANK_UART4_TX,
 		.pin = GPIO_UART4_TX,
-		.cnf = GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
 	},
 	.rts = {	// RTS
 		.port = GPIOB,
 		.pin = GPIO6,
-		.cnf = GPIO_CNF_OUTPUT_PUSHPULL,
 	},
 	.dtr = {	// DTR
 		.port = GPIOB,
 		.pin = GPIO5,
-		.cnf = GPIO_CNF_OUTPUT_PUSHPULL,
 	},
 }};
 
-void dfu_detach(void)
-{
-	dfu_jump_app_if_valid();
+void init_ports(void) {
+	uint8_t i, j;
 
-        /* USB device must detach, we just reset... */
-	scb_reset_system();
-}
-
-int main(void)
-{
-	uint8_t i;
-	uint8_t serial_board = false;
-	
-	/* ensure serialno is linked in */
-	if ((board_serial_no[0] == '1') && (board_serial_no[1] == '2') && (board_serial_no[2] == '4')) {
-		serial_board = true;
-	}
-	
-	dfu_protect(DFU_MODE);
-
-	rcc_clock_setup_in_hse_8mhz_out_72mhz();
-	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
-	systick_set_reload(900000);
-	
 	// Enable clocks for (GPIOA, GPIOB, GPIOC, GPIOD)
 	rcc_peripheral_enable_clock(&RCC_APB2ENR,
 	    RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN |
 	    RCC_APB2ENR_IOPCEN | RCC_APB2ENR_IOPDEN);
-	if (serial_board) {
-		// *** Special pin setup for UARTS and UART LEDs ***
-		// Enable clocks for (USART1, AFIO)
-		rcc_peripheral_enable_clock(&RCC_APB2ENR,
-		    RCC_APB2ENR_USART1EN | RCC_APB2ENR_AFIOEN);
-		// Enable clocks all four uarts (USART2, USART3, UART4)
-		rcc_peripheral_enable_clock(&RCC_APB1ENR,
-			RCC_APB1ENR_USART2EN | RCC_APB1ENR_USART3EN |
-			RCC_APB1ENR_UART4EN);
-	}
 
 	// Set all port pins to input pull-down
 	gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN, GPIO_ALL);
@@ -175,24 +137,52 @@ int main(void)
 	gpio_set_mode(GPIO_BANK_JTCK_SWCLK, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN, GPIO_JTCK_SWCLK);
 	gpio_clear(GPIO_BANK_JTCK_SWCLK, GPIO_JTCK_SWCLK);
 
+	// *** Special pin setup for UARTS and UART LEDs ***
+	for(j=0; j<sizeof(serial_board_serial_numbers)/(SERIAL_NUM_NUM_CHARS*sizeof(char)); j++) {
+		if ((board_serial_no[0] == serial_board_serial_numbers[j][0]) &&
+			(board_serial_no[1] == serial_board_serial_numbers[j][1]) &&
+			(board_serial_no[2] == serial_board_serial_numbers[j][2]))
+		{
+			for(i=0; i<4; i++) {
+				gpio_set_mode(uarts[i].tx.port, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, uarts[i].tx.pin);
+				gpio_set_mode(uarts[i].rts.port, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, uarts[i].rts.pin);
+				gpio_set_mode(uarts[i].dtr.port, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, uarts[i].dtr.pin);
+				gpio_set(uarts[i].tx.port, uarts[i].tx.pin);
+				gpio_set(uarts[i].rts.port, uarts[i].rts.pin);
+				gpio_set(uarts[i].dtr.port, uarts[i].dtr.pin);
+			}
+			break;
+		}
+	}
+};
+
+void dfu_detach(void)
+{
+	dfu_jump_app_if_valid();
+
+        /* USB device must detach, we just reset... */
+	scb_reset_system();
+}
+
+int main(void)
+{
+	dfu_protect(DFU_MODE);
+
+	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+
+	// set initial state of port pins
+	init_ports();
+
+	// setup systic
+	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
+	systick_set_reload(900000);
+	
+	// Setup USB hardware
 	rcc_periph_clock_enable(RCC_USB);
 	gpio_set_mode(GPIOA, GPIO_MODE_INPUT, 0, GPIO8);
 
 	gpio_set_mode(GPIOB, GPIO_MODE_INPUT,
 			GPIO_CNF_INPUT_FLOAT, GPIO2 | GPIO10);
-
-	if (serial_board) {
-		// *** Special pin setup for UARTS and UART LEDs ***
-		//UART Setup
-		for(i=0; i<4; i++) {
-			gpio_set_mode(uarts[i].tx.port, GPIO_MODE_OUTPUT_50_MHZ, uarts[i].tx.cnf, uarts[i].tx.pin);
-			gpio_set_mode(uarts[i].rts.port, GPIO_MODE_OUTPUT_50_MHZ, uarts[i].rts.cnf, uarts[i].rts.pin);
-			gpio_set_mode(uarts[i].dtr.port, GPIO_MODE_OUTPUT_50_MHZ, uarts[i].dtr.cnf, uarts[i].dtr.pin);
-			usart_set_baudrate(uarts[i].usart, 9600);
-			usart_set_mode(uarts[i].usart, USART_MODE_TX_RX);
-			usart_enable(uarts[i].usart);
-		}
-	}
 
 	dfu_init(&stm32f103_usb_driver, DFU_MODE);
 
@@ -202,4 +192,3 @@ int main(void)
 
 	dfu_main();
 }
-
